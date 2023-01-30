c22066b36b8da1779327323629e5aec60c673efa
saracsilva <saracarolinasilva@gmail.com>
2023-01-30 18:16:48 +0100

    added style to the Search component title and start the form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
+import svgBackground from "./assets/background_shape.svg";
 
 function App() {
   return (
-    <div className="app  ">
+    <div className="app flex flex-col  content-center  ">
       <Header />
-      <Search />
+      <Search className="w-5/6 mx-auto" />
+
+      <img
+        src={svgBackground}
+        alt="background"
+        className="blur-3xl  opacity-50 -z-1   w-1/5 "
+      />
     </div>
   );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import play from "../assets/play_icon.svg";
 
 function Search() {
-  return <div className="text-9xl">Search</div>;
+  return (
+    <>
+      <div className="grid grid-cols-5  justify-items-center items-center">
+        <div className="col-start-2 text-9xl col-span-2">Search</div>
+        <div className="col-start-4 flex justify-between   text-xl py-3 px-10 border border-solid border-dark-blue rounded-full gap-5">
+          <img src={play} alt="icon play" className="w-5" />
+          <p> /sɜːt͡ʃ/</p>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <form>
+          <input
+            type="search"
+            placeholder="Search"
+            className="focus:ring-indigo-500 "
+          ></input>
+          <input type="submit"></input>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Search;
