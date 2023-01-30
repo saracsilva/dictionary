fe2846813b2c31521321dcb2c12cafd8ac91aae2
saracsilva <saracarolinasilva@gmail.com>
2023-01-30 16:04:53 +0100

    tailwind config, header and search components created

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="p-5 ">
+      my<span className="font-black">Dictionary</span>
+    </div>
+  );
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        "dark-blue": "#263d42",
+        "light-blue": "#7698b3",
+        "light-yellow": "#f5f1e0",
+        grey: "#dcdde4",
+        "light-grey": "#f8f6f1",
+      },
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+
+      content: {
+        play: "url('./assets/play_icon.svg')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+    fontSize: {
+      sm: ["14px", "20px"],
+      base: ["16px", "24px"],
+      lg: ["20px", "28px"],
+      xl: ["24px", "32px"],
+      "9xl": [
+        "15rem",
+        {
+          fontWeight: "700",
+        },
+      ],
+    },
+  },
+  plugins: [],
+};
